3d7d7cb8d58593de965f496347cac89ac35a2248
berkcinar <44802680+berkcinar@users.noreply.github.com>
2020-02-11 09:36:35 +0300

    Update get_ikitelli.py

--- get_ikitelli.py
+++ get_ikitelli.py
@@ -11,10 +11,14 @@ import requests
 from darksky import forecast
 from datetime import datetime as dt,timedelta
 import pandas as pd
+import os.path
+import numpy
+import time
 
 class Solar_Generation():
 
     def get_ikitelli_generation(self):
+        generation_df = pd.DataFrame()
         # api-endpoint
         URL = "https://data.ibb.gov.tr/api/3/action/datastore_search?resource_id=52afa9a3-2ea1-420b-a783-505cfe635ece"
         r = requests.get(url=URL)
@@ -24,9 +28,21 @@ class Solar_Generation():
         r = requests.get(url=URL, params=PARAMS)
         data = r.json()
         data['result']['total']
-        print(data['result']['records'])
         for i in data['result']['records']:
-            print(i['Tarih'],i['Uretim (kWh)'])
+            # generation_df = generation_df.append({'timeslot': dt.strptime(i['Tarih'].replace("T", " ")[:13] + ':00:00', '%Y-%m-%d %H:%M:%S'),'uretim': i['Uretim (kWh)'],},ignore_index=True)
+            generation_df = generation_df.append({'timeslot': dt.strptime(i['Tarih'].replace("T", " "), '%Y-%m-%d %H:%M:%S'),'uretim': i['Uretim (kWh)'],},ignore_index=True)
+            # df2 = df2.append({'timeslot': dt.fromtimestamp((i['Tarih']).strftime('%Y-%m-%d %H:%M:%S')),'uretim': i['Uretim (kWh)'],},ignore_index=True)
+
+        #eksik değerleri tamamlamam lazım.
+        print(generation_df)
+        # generation_df=self.check_and_fill_zeros(generation_df)
+        generation_df.set_index('timeslot', inplace=True)
+
+        generation_df=generation_df.groupby('timeslot').mean()
+        # generation_df=generation_df.groupby(pd.TimeGrouper(freq='15Min')).aggregate(numpy.sum)
+        print(generation_df)
+
+        return generation_df
 
     # def get_solar_radiaton_NASA(self):
     #     # URL = "https://api.darksky.net/forecast/992bb820ba82528fffc192c56bf6921e/41.086903,28.765224"
@@ -74,62 +90,102 @@ class Solar_Generation():
             print(i['windSpeed'])
         print(dt.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'))
 
-    def get_from_darksky_now(self):
-        d={}
-        df2=pd.DataFrame()
+    def get_from_darksky_now(self,startdate=None,enddate=None):
+        darksky_df=pd.DataFrame()
         api_key = '992bb820ba82528fffc192c56bf6921e'
         energypool_latitude=41.086400
         energypool_longitude=29.020917
 
-        print(dt(2020, 1, 28, 12))
-        print(dt(2020, 1, 28, 12)+timedelta(hours=10))
-
-
+        # print(dt(2020, 1, 28, 12))
+        # print(dt(2020, 1, 28, 12)+timedelta(hours=10))
 
         # t = (dt(2020, 1, 28, 12).isoformat())
+        # t=(dt(2020, 1, 29, 12)+timedelta(hours=15)).isoformat()
+        startdate=dt(2019, 5, 1)
+        enddate=dt(2019, 5, 31)
+        # enddate = dt(2019,5,2)
+        t = dt(2019, 5, 31, 1).isoformat()
+        while startdate<enddate:
+            print(startdate)
+            energypool = forecast(api_key, energypool_latitude, energypool_longitude,units='si',lang='tr',time=startdate.isoformat())
+            for i in energypool['hourly']['data']:
+                darksky_df = darksky_df.append({'timeslot': dt.fromtimestamp(i['time']).strftime('%Y-%m-%d %H:%M:%S'),
+                                  'summary': i['summary'],
+                                  'precipIntensity': i['precipIntensity'],
+                                  'precipProbability': i['precipProbability'],
+                                  'temperature': i['temperature'],
+                                  'apparentTemperature': i['apparentTemperature'],
+                                  'dewPoint': i['dewPoint'],
+                                  'humidity': i['humidity'],
+                                  # 'pressure': i['pressure'],
+                                  'windSpeed': i['windSpeed'],
+                                  'windBearing': i['windBearing'],
+                                  'cloudCover': i['cloudCover'],
+                                  'uvIndex': i['uvIndex'],
+                                  'visibility': i['visibility'],
+                                  },
+                                 ignore_index=True)
 
-        t=(dt(2020, 1, 28, 12)+timedelta(hours=15)).isoformat()
-        t = dt(2013, 5, 6, 12).isoformat()
-        energypool = forecast(api_key, energypool_latitude, energypool_longitude,units='si',lang='tr',time=t)
-        for i in energypool['hourly']['data']:
-            print(dt.fromtimestamp(i['time']).strftime('%Y-%m-%d %H:%M:%S'))
-            print(i['summary'])
-            df2 = df2.append({'timeslot': dt.fromtimestamp(i['time']).strftime('%Y-%m-%d %H:%M:%S'),
-                              'summary': i['summary'],
-                              'precipIntensity': i['precipIntensity'],
-                              'precipProbability': i['precipProbability'],
-                              'temperature': i['temperature'],
-                              'apparentTemperature': i['apparentTemperature'],
-                              'dewPoint': i['dewPoint'],
-                              'humidity': i['humidity'],
-                              # 'pressure': i['pressure'],
-                              'windSpeed': i['windSpeed'],
-                              'windBearing': i['windBearing'],
-                              'cloudCover': i['cloudCover'],
-                              'uvIndex': i['uvIndex'],
-                              'visibility': i['visibility'],
-                              },
-                             ignore_index=True)
+            startdate=startdate+timedelta(hours=24)
 
-        print(df2.dtypes)
-        print(df2.head())
-        print(df2['timeslot'])
+        # darksky_df.set_index('timeslot', inplace=True)
+        darksky_df.to_csv('darksky.csv', sep=',', encoding='utf-8',header='true')
+        return darksky_df
+
+    def csv_to_df(self):
+
+        darksky_pd = pd.read_csv("darksky.csv",encoding='utf-8')
+        darksky_pd.set_index('timeslot', inplace=True)
+        return darksky_pd
+
+    def merge_dataframe(self):
+        generation_df=self.get_ikitelli_generation()
+        if os.path.isfile('darksky.csv'):
+            print("File exist")
+            darksky_df = self.csv_to_df()
+        else:
+            print("File not exist")
+            darksky_df = self.get_from_darksky_now()
+
+        merged_inner = pd.merge(left=generation_df, right=darksky_df, left_index=True, right_index=True)
+        merged_inner.to_csv('merged_inner.csv', sep=',', encoding='utf-8', header='true')
 
 
+    def check_and_fill_zeros(self,generation_df):
+        liste = pd.DataFrame()
+        print(generation_df['timeslot'])
+
+        print(generation_df['timeslot'].iloc[0],generation_df['timeslot'].iloc[-1])
 
 
+        startdate=generation_df['timeslot'].iloc[0] - timedelta(hours=generation_df['timeslot'].iloc[0].hour, minutes=generation_df['timeslot'].iloc[0].minute)
+
+        enddate=generation_df['timeslot'].iloc[-1]-timedelta(hours=generation_df['timeslot'].iloc[-1].hour,minutes=generation_df['timeslot'].iloc[-1].minute)
+
+        print(startdate,enddate)
+
+        idx = pd.date_range(startdate, periods=(enddate+timedelta(hours=1)-startdate).total_seconds()*4/3600, freq='15Min')
+
+        for timeslot in idx:
+            if timeslot in generation_df.values:
+                pass
+            else:
+                generation_df=generation_df.append({'timeslot': timeslot,'uretim': 0 }, ignore_index=True)
 
 
+        generation_df.sort_values("timeslot", axis=0, ascending=True,
+                         inplace=True, na_position='first')
 
-
-
-        # time=energypool['hourly']['time']
-        # print(dt.fromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'))
-
+        return generation_df
 
 
     def run(self):
         # self.get_from_darksky()
-        self.get_from_darksky_now()
-
+        # self.get_from_darksky_now()
+        self.get_ikitelli_generation()
+        # self.get_from_darksky_now()
+        # self.merge_dataframe()
+        # self.csv_to_df()
+        # self.get_from_darksky_now()
+        # self.check_and_fill_zeros()
 Solar_Generation().run()
